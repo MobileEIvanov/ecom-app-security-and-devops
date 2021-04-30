76390b24ea516bb8853aaf63f12d652bafba6d6b
E Ivanov <emil.iv.ivanov@gmail.com>
2021-04-30 19:29:11 +0300

    Commit project review and Jenkins file

---
+++ starter_code/src/main/java/com/example/demo/security/JWTAuthVerificationFilter.java
@@ -0,0 +1,53 @@
+package com.example.demo.security;
+
+import com.auth0.jwt.JWT;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
+public class JWTAuthVerificationFilter extends BasicAuthenticationFilter {
+
+    public JWTAuthVerificationFilter(AuthenticationManager authManager) {
+        super(authManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+      throws IOException, ServletException {
+        String header = req.getHeader(Constants.TOKEN_TYPE);
+
+        if (header == null || !header.startsWith(Constants.TOKEN_TYPE)) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
+        String token = req.getHeader(Constants.HEADER);
+        if (token != null) {
+            String user = JWT.require(HMAC512(Constants.SECRET.getBytes())).build()
+              .verify(token.replace(Constants.TOKEN_TYPE, ""))
+              .getSubject();
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+            return null;
+        }
+        return null;
+    }
+}
