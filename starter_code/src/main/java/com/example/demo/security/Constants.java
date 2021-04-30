e3e3dcbe5fb436ee4a0abb6052a7617660c5e469
E Ivanov <emil.iv.ivanov@gmail.com>
2021-04-30 19:40:19 +0300

    Changed project setup

--- starter_code/src/main/java/com/example/demo/security/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.security;
-
-public interface Constants {
-    String SECRET = "secretKey";
-    long EXPIRATION_TIME = 864_000_000;
-    String TOKEN_TYPE = "Bearer ";
-    String HEADER = "Authorization";
-    String CREATE_URL = "/api/user/create";
-}
